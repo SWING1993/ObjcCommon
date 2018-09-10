50221894440c2e98faca254d5277e7fcac406dac
SWING1993 <songguohua10446627@gmail.com>
2018-09-10 20:34:39 +0800

    初始化

---
+++ Moments/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Moments
+//
+//  Created by 宋国华 on 2018/9/10.
+//  Copyright © 2018年 songguohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Moments/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Moments
+//
+//  Created by 宋国华 on 2018/9/10.
+//  Copyright © 2018年 songguohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
